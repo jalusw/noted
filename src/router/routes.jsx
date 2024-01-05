1caf5f0f5c61fec52aab0ec91e027622ea743f1b
jalusw <jalusatrianiwijaya1@gmail.com>
2024-01-05 20:30:07 +0800

    add home page to route

--- src/router/routes.jsx
+++ src/router/routes.jsx
@@ -1,9 +1,10 @@
-import App from "../App";
+import Home from "../pages/home/Home";
+import { getInitialData } from "../utils";
 
 const routes = [
   {
     path: "/",
-    element: <App />,
+    element: <Home data={getInitialData()} />,
   },
 ];
 export default routes;
