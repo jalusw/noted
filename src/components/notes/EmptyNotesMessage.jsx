c06d5b5e86c65e289c059517d64acf56124c875d
jalusw <jalusatrianiwijaya1@gmail.com>
2024-01-05 20:29:36 +0800

    create empty notes message component

---
+++ src/components/notes/EmptyNotesMessage.jsx
@@ -0,0 +1,9 @@
+function EmptyNotesMessage() {
+  return (
+    <div className="text-center">
+      <p>You Currently do not have any notes.</p>
+    </div>
+  );
+}
+
+export default EmptyNotesMessage;
